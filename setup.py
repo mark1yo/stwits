9905e08a35e9f21c72360f1cbcafb218c92ec91d
Mark Yosef <markyo91@gmail.com>
2021-01-16 03:53:29 +0200

    pypi preparation

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 with open('README.md') as readme_file:
     README = readme_file.read()
@@ -13,21 +13,18 @@ setup_args = dict(
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
     license='MIT',
-    packages=find_packages(),
+    packages=['stwits'],
     author='Mark Yosef Joseph',
     author_email='markio@post.bgu.ac.il',
     keywords=['Stock', 'Stocks', 'Twit', 'Twits', 'Tweet', 'Tweets', 'Stocktwits',
               'Stocktwits.com', 'www.Stocktwits.com', 'finance', 'Investing', 'Trading', 
               'Bonds', 'Equities', 'Funds', 'Securities', 'Commodities', 'Crypto'],
+    install_requires = ['requests', 'datetime', 'json'],
     url='https://github.com/mark1yo/stwits',
     download_url='https://pypi.org/project/stwits/'
 )
 
-install_requires = [
-    'requests',
-    'datetime',
-    'json'
-]
 
 if __name__ == '__main__':
-    setup(**setup_args, install_requires=install_requires)
+    setup(**setup_args)
+

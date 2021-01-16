331ff00b61fb4ce3aec0752695eac489ce3a543b
Mark Yosef <markyo91@gmail.com>
2021-01-16 04:13:09 +0200

    setup fix

--- setup.py
+++ setup.py
@@ -1,21 +1,13 @@
 from distutils.core import setup
 
-with open('README.md') as readme_file:
-    README = readme_file.read()
-
-with open('HISTORY.md') as history_file:
-    HISTORY = history_file.read()
-
 setup_args = dict(
     name='stwits',
-    version='0.0.1',
     description='Python client for www.stocktwits.com API',
-    long_description_content_type="text/markdown",
-    long_description=README + '\n\n' + HISTORY,
+    version='0.0.1',
     license='MIT',
-    packages=['stwits'],
     author='Mark Yosef Joseph',
     author_email='markio@post.bgu.ac.il',
+    packages=['stwits'],
     keywords=['Stock', 'Stocks', 'Twit', 'Twits', 'Tweet', 'Tweets', 'Stocktwits',
               'Stocktwits.com', 'www.Stocktwits.com', 'finance', 'Investing', 'Trading', 
               'Bonds', 'Equities', 'Funds', 'Securities', 'Commodities', 'Crypto'],
